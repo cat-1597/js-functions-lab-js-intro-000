d194d05393153b8d3b3338d281f41c8a3b44b35a
Cat <cchiong1597@gmail.com>
2017-10-08 19:38:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 function happyHolidays() {
 return "Happy holidays!"
 }
-function happyHolidaysTo(string) {
+function happyHolidaysTo(name) {
   return 'Happy holidays, ${name}!'
 }
-function happyHolidaysTo(holiday,name) {
+function happyHolidaysTo(holiday, name) {
   return 'Happy ${holiday}, ${name}!'
 }
 function holidayCountdown(days, holiday) {
